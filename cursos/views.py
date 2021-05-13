ab467edea56e43456268dcc79dcb13fbe2b1e486
nunescarol <59810994+nunescarol@users.noreply.github.com>
2021-05-13 10:11:57 -0300

    função de upar arquivo organizada

--- cursos/views.py
+++ cursos/views.py
@@ -2,13 +2,15 @@ from django.http import Http404
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from .forms import CreateCourseForm, CreateActivityForm, CreateModuleForm, AddFileForm, AddImageForm, Escolha, AddTextForm, AddVideoForm
-from django.utils.text import slugify
-from .models import Course, Module, Content, Activity, Post
 from django.urls import path, reverse, reverse_lazy
 from django.forms.models import modelform_factory
 from django.apps import apps
 from django.db.models import Q
+from django.utils.text import slugify
+
+from .forms import CreateCourseForm, CreateActivityForm, CreateModuleForm, AddFileForm, AddImageForm, Escolha, AddTextForm, AddVideoForm
+from .models import Course, Module, Content, Activity, Post
+
 
 
 def inicio(request):
@@ -34,9 +36,14 @@ def criar(request):
                 curso = Course(owner=owner, subject=subject, title=title, overview=overview, slug=slug)
                 curso.save()
                 return redirect('/genus/inicio/')
+
+            else:
+                #handle invalid form
+                form = CreateCourseForm()
+                return render(request, 'createCurso.html', {'form': form})
         else:
             form = CreateCourseForm()
-        return render(request, 'createCurso.html', {'form': form})
+            return render(request, 'createCurso.html', {'form': form})
     else:
         return redirect('/')
 
@@ -59,8 +66,8 @@ def resumo(request, curso_slug):
         return redirect('/')
 
 def curso(request, curso_slug):
-    dono=False
     if request.user.is_authenticated:
+        dono=False
         try:
             c= Course.objects.get(slug=curso_slug)
         except Course.DoesNotExist:
@@ -72,16 +79,13 @@ def curso(request, curso_slug):
         return redirect('/')
 
 def modulos(request, curso_slug):
-    dono=False
     if request.user.is_authenticated:
+        dono=False
         try:
             c= Course.objects.get(slug=curso_slug)
             m= Module.objects.filter(course=c)
             m= m.filter( Q(not_instance_of=Activity) & Q(not_instance_of=Post))
-            a = Activity.objects.filter(module__in=m)
-            p = Post.objects.filter(module__in=m)
             ap = Module.objects.filter(Q(instance_of=Activity) | Q(instance_of=Post))
-            # ap = Module.objects.filter(Q(Activity___module = m) | Q(Post___module = m))
             ap= ap.filter(Q(Activity___module__in = m) | Q(Post___module__in = m))
         except Course.DoesNotExist:
             raise Http404("Encontramos um erro")
@@ -89,22 +93,21 @@ def modulos(request, curso_slug):
             m=None
         except Activity.DoesNotExist:
             a=None
+
         if(request.user==c.owner):
             dono=True
+
         return render(request, 'modulosCurso.html', {'curso':c, 'dono': dono, 'modulos': m, 'atividades_posts': ap})
     else:
         return redirect('/')
 
 def criar_modulo(request, curso_slug):
-    dono=False
-    try:
-        c= Course.objects.get(slug=curso_slug)
-    except Course.DoesNotExist:
-        raise Http404("Encontramos um erro")
-    if(request.user==c.owner):
-            dono=True
     if request.user.is_authenticated:
-        if dono:
+        try:
+            c= Course.objects.get(slug=curso_slug)
+        except Course.DoesNotExist:
+            raise Http404("Encontramos um erro")
+        if(request.user==c.owner):
             if request.method == 'POST':
                 form = CreateModuleForm(request.POST)
                 if form.is_valid():
@@ -113,14 +116,14 @@ def criar_modulo(request, curso_slug):
                     form.save()    
                     return redirect('/genus/'+curso_slug+'/modulos/')
             else:
-
                 form = CreateModuleForm()
-            return render(request, 'createModule.html', {'form': form})
+                return render(request, 'createModule.html', {'form': form})
         else:
-            print("opaopa")
-            return redirect('/genus/inicio/')
+            print("Você não tem permissão para criar um módulo nesse curso")
+            return redirect('/genus/'+curso_slug)
     else:
         return redirect('/')
+
 
 def exibir_modulo(request, curso_slug, modulo_id):
     if request.user.is_authenticated:
@@ -180,7 +183,7 @@ def exibir_atividade_post(request, curso_slug, modulo_id, atividade_post_id):
         try:
             c = Course.objects.get(slug=curso_slug)
             m = Module.objects.get(pk=modulo_id)
-            ap = Module.objects.get(Q(Activity___pk = atividade_post_id))
+            ap = Module.objects.get(Q(Activity___pk = atividade_post_id) | Q(Post___pk = atividade_post_id))
         except Course.DoesNotExist:
             raise Http404("Ops, esse curso não existe")
         except Module.DoesNotExist:
@@ -193,84 +196,63 @@ def exibir_atividade_post(request, curso_slug, modulo_id, atividade_post_id):
     else:
         return redirect('/')
 
+tipo_form = {
+  "1": AddTextForm,
+  "2": AddImageForm,
+  "3": AddVideoForm,
+  "4": AddFileForm,
+}
 
-def adicionar_arquivo4(request, curso_slug, modulo_id, atividade_post_id):
-    dono=False
-    try:
-        m = Module.objects.get(pk=modulo_id)
-    except Module.DoesNotExist:
-        raise Http404("Ops, esse módulo ou conteudo não existe")
-
-    try:
-        c = Course.objects.get(slug=curso_slug)
-    except Course.DoesNotExist:
-        raise Http404("Ops, esse curso não existe")
-    
+def adicionar_arquivo(request, curso_slug, modulo_id, atividade_post_id):
     if request.user.is_authenticated:
+        dono=False
+        try:
+            c = Course.objects.get(slug=curso_slug)
+        except Course.DoesNotExist:
+            raise Http404("Ops, esse curso não existe")
+
+        try:
+            m = Module.objects.get(pk=modulo_id)
+        except Module.DoesNotExist:
+            raise Http404("Ops, esse módulo ou conteudo não existe")
+        
         if request.user==c.owner:
             dono=True
         else:
             print("voce nao tem permissao para fazer isso.")
-            return redirect('/genus/inicio/')
-        
+            return redirect('/genus/'+curso_slug+'/'+str(modulo_id)+'/'+str(atividade_post_id)+'/')
         
         if request.method == 'POST':
-            form = AddFileForm(request.POST)
+            form1 = EscolhaTipo(request.POST)
 
-            if form.is_valid():
-                record = form.save(commit=False)
-                record.owner=request.user
-                form.save()
+            if form1.is_valid():
+                
+                if('arquivo' in request.POST):
+                    tipo = form1.cleaned_data.get('escolha')
+                    form2=tipo_form[tipo](request.POST, request.FILES)
 
-                Content.objects.create(module=m,item=record)
+                    if form2.is_valid():
+                        record = form2.save(commit=False)
+                        record.owner=request.user
+                        form2.save()
 
-                # return redirect('/genus/'+curso_slug+'/'+str(modulo_id)+'/'+str(atividade_post_id)+'/')
+                        Content.objects.create(module=m,item=record)
+                        return redirect('/genus/'+curso_slug+'/'+str(modulo_id)+'/'+str(atividade_post_id)+'/')
+                    else:
+                        # handle invalid form
+                        return redirect('/genus/'+curso_slug+'/'+str(modulo_id)+'/'+str(atividade_post_id)+'/')
+
+                else:
+                    tipo = form1.cleaned_data.get('escolha')
+                    form2=tipo_form[tipo]()
+                    return render(request, 'addArquivo2.html', {'form1': form1, 'form2': form2})
+            else:
+                # handle invalid form
                 return redirect('/genus/'+curso_slug+'/'+str(modulo_id)+'/'+str(atividade_post_id)+'/')
 
         else:
-            form = AddFileForm()
-            return render(request, 'addArquivo.html', {'form': form})
-            
-    else:
-        return redirect('/')
-
-def adicionar_imagem(request, curso_slug, modulo_id, atividade_post_id):
-    dono=False
-    try:
-        m = Module.objects.get(pk=modulo_id)
-    except Module.DoesNotExist:
-        raise Http404("Ops, esse módulo ou conteudo não existe")
-
-    try:
-        c = Course.objects.get(slug=curso_slug)
-    except Course.DoesNotExist:
-        raise Http404("Ops, esse curso não existe")
-    
-    if request.user.is_authenticated:
-        if request.user==c.owner:
-            dono=True
-        else:
-            print("voce nao tem permissao para fazer isso.")
-            return redirect('/genus/inicio/')
-        
-        
-        if request.method == 'POST':
-            form = AddImageForm(request.POST, request.FILES)
-
-            if form.is_valid():
-                record = form.save(commit=False)
-                record.owner=request.user
-                form.save()
-
-                Content.objects.create(module=m,item=record)
-
-                # return redirect('/genus/'+curso_slug+'/'+str(modulo_id)+'/'+str(atividade_post_id)+'/')
-                return redirect('/genus/'+curso_slug+'/'+str(modulo_id)+'/'+str(atividade_post_id)+'/')
-
-        else:
-            form = AddImageForm()
-            return render(request, 'addArquivo.html', {'form': form})
-            
+            form1 = EscolhaTipo()
+            return render(request, 'addArquivo2.html', {'form1': form1, 'form2': None})
     else:
         return redirect('/')
 
@@ -323,80 +305,3 @@ def adicionar_imagem(request, curso_slug, modulo_id, atividade_post_id):
 #                 Content.objects.create(module=self.module,item=obj)
 #             return redirect('module_content_list', self.module.id)
 #         return self.render_to_response({'form': form, 'object': self.obj})
-
-tipo_form = {
-  "1": AddTextForm,
-  "2": AddImageForm,
-  "3": AddVideoForm,
-  "4": AddFileForm,
-}
-
-def escolha():
-    pass
-
-def adicionar_arquivo(request, curso_slug, modulo_id, atividade_post_id):
-    dono=False
-    try:
-        m = Module.objects.get(pk=modulo_id)
-    except Module.DoesNotExist:
-        raise Http404("Ops, esse módulo ou conteudo não existe")
-
-    try:
-        c = Course.objects.get(slug=curso_slug)
-    except Course.DoesNotExist:
-        raise Http404("Ops, esse curso não existe")
-    
-    if request.user.is_authenticated:
-        if request.user==c.owner:
-            dono=True
-        else:
-            print("voce nao tem permissao para fazer isso.")
-            return redirect('/genus/inicio/')
-    
-        if request.method == 'POST':
-            form1 = Escolha(request.POST)
-
-            if form1.is_valid():
-                
-                if('arquivo' in request.POST):
-                    tipo = form1.cleaned_data.get('escolha')
-                    form2=tipo_form[tipo](request.POST, request.FILES)
-
-                    if form2.is_valid():
-                        record = form2.save(commit=False)
-                        record.owner=request.user
-                        form2.save()
-
-                        Content.objects.create(module=m,item=record)
-                        return redirect('/genus/'+curso_slug+'/'+str(modulo_id)+'/'+str(atividade_post_id)+'/')
-                    else:
-                        print("form2 invalido")
-                        return redirect('/genus/teste/')
-                    
-
-                    # else:
-                    #     return render(request, 'addArquivo2.html', {'form1': form1, 'form2': None})
-
-                else:
-                    tipo = form1.cleaned_data.get('escolha')
-                    form2=tipo_form[tipo]()
-                    return render(request, 'addArquivo2.html', {'form1': form1, 'form2': form2})
-
-
-                # record = form.save(commit=False)
-                # record.owner=request.user
-                # form.save()
-
-                # Content.objects.create(module=m,item=record)
-                # return redirect('/genus/'+curso_slug+'/'+str(modulo_id)+'/'+str(atividade_post_id)+'/')
-                # return redirect('/genus/teste/')
-            else:
-                print("form1 invalido")
-                print(request.POST)
-                return redirect('/genus/inicio/')
-
-        else:
-            form1 = Escolha()
-            return render(request, 'addArquivo2.html', {'form1': form1, 'form2': None})
-    else:
-        return redirect('/')
